f42c8c17e0571d763a74e69228fcbc3ab7558fca
TheTaoueille <alan.rousic@orange.fr>
2021-02-01 00:03:27 +0100

    Ajout de la permission pour le GPS, premier test pour le GPS -> SearchActivity.java, ajout des images + adapter sur les Good (GoodContent et GoodFragment et MyGoodRecyclerViewAdapter)

---
+++ app/src/main/java/fr/univpau/android/quelpriximmo/PositionManager.java
@@ -0,0 +1,26 @@
+package fr.univpau.android.quelpriximmo;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+
+public class PositionManager {
+
+    public static Location getPositionViaGPS(Context context) throws SecurityException {
+        LocationManager lm;
+        Location position = null;
+        String provider;
+
+        lm = (LocationManager) context.getSystemService(context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        provider = lm.getBestProvider(criteria, true);
+
+        if (provider != null) {
+            position = lm.getLastKnownLocation(provider);
+        }
+        return position;
+    }
+}
--- app/src/main/java/fr/univpau/android/quelpriximmo/SearchActivity.java
+++ app/src/main/java/fr/univpau/android/quelpriximmo/SearchActivity.java
@@ -1,7 +1,12 @@
 package fr.univpau.android.quelpriximmo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+
+import static fr.univpau.android.quelpriximmo.PositionManager.getPositionViaGPS;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Location position = getPositionViaGPS(this);
+        Log.i("GPS","Latitude = " + position.getLatitude());
+        Log.i("GPS", "Longitude" + position.getLongitude());
+        /*
+        // http://api.cquest.org/dvf?lat=48.85&lon=2.35&dist=100
+        URL url = new URL("https://openclassrooms.com/");
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } finally {
+            urlConnection.disconnect();
+        }
+         */
     }
 }
