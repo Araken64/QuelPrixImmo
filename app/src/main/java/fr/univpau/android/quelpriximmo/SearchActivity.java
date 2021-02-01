b58ac20a83314856a8717b2f63604d143bbf888c
Araken64 <elian.araken@gmail.com>
2021-02-01 04:16:13 +0100

    Use onClick listener on search button

--- app/src/main/java/fr/univpau/android/quelpriximmo/SearchActivity.java
+++ app/src/main/java/fr/univpau/android/quelpriximmo/SearchActivity.java
@@ -1,10 +1,12 @@
 package fr.univpau.android.quelpriximmo;
 
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import fr.univpau.android.quelpriximmo.listeners.ButtonSearchListener;
 
 import static fr.univpau.android.quelpriximmo.PositionManager.getPositionViaGPS;
 
@@ -14,10 +16,13 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        Button button_search = (Button) findViewById(R.id.button_search);
+        button_search.setOnClickListener(new ButtonSearchListener(this));
+
 
         Location position = getPositionViaGPS(this);
-        Log.i("GPS","Latitude = " + position.getLatitude());
-        Log.i("GPS", "Longitude" + position.getLongitude());
+        // Log.i("GPS","Latitude = " + position.getLatitude());
+        // Log.i("GPS", "Longitude" + position.getLongitude());
         /*
         // http://api.cquest.org/dvf?lat=48.85&lon=2.35&dist=100
         URL url = new URL("https://openclassrooms.com/");
