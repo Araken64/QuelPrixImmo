e3f3ebb59959a540e64459b30a668692cc5426ca
TheTaoueille <alan.rousic@orange.fr>
2021-02-02 00:05:13 +0100

    Activité Settings terminée -> avec seekBar et bouton retour vers search

--- app/src/main/java/fr/univpau/android/quelpriximmo/SettingsActivity.java
+++ app/src/main/java/fr/univpau/android/quelpriximmo/SettingsActivity.java
@@ -1,30 +1,61 @@
 package fr.univpau.android.quelpriximmo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import androidx.appcompat.app.ActionBar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+    int distance;
+    SeekBar bar;
+    TextView distance_display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment())
-                .commit();
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
+        bar = findViewById(R.id.distance_seekbar);
+        distance_display = findViewById(R.id.distance_display);
+        bar.setMax(1500);
+        distance = bar.getProgress() + 500;
+        distance_display.setText(String.valueOf(distance));
+
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                                           @Override
+                                           public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                                           }
+
+                                           @Override
+                                           public void onStartTrackingTouch(SeekBar seekBar) {
+
+                                           }
+
+                                           @Override
+                                           public void onStopTrackingTouch(SeekBar seekBar) {
+                                               distance = bar.getProgress() + 500;
+                                               distance_display.setText(String.valueOf(distance));
+                                           }
+                                       }
+        );
+
+        Button button_valider = findViewById(R.id.button_valider);
+        button_valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getPreferences(Activity.MODE_PRIVATE);
+                SharedPreferences.Editor ed = pref.edit();
+                ed.putInt("distance", distance);
+                Intent intent = new Intent(view.getContext(), SearchActivity.class);
+                view.getContext().startActivity(intent);
+                finish();
+            }
+        });
     }
 }
