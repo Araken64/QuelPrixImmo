f0696bcb62c15c7e4e666f020e06d3f060a05b0b
Araken64 <elian.araken@gmail.com>
2021-02-01 04:16:35 +0100

    get Intent extra values

--- app/src/main/java/fr/univpau/android/quelpriximmo/ResultsActivity.java
+++ app/src/main/java/fr/univpau/android/quelpriximmo/ResultsActivity.java
@@ -9,5 +9,10 @@ public class ResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        String type_local = this.getIntent().getStringExtra("type_local");
+        String date_nature = this.getIntent().getStringExtra("date_nature");
+        String nombre_pieces_principales = this.getIntent().getStringExtra("nombre_pieces_principales");
+
+        this.findViewById(R.id.mutation_list);
     }
 }
