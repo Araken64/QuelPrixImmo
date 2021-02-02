b7b090fc940237d31650421380eb2409790c7fd8
Araken64 <elian.araken@gmail.com>
2021-02-02 02:08:41 +0100

    toString method improve + addJson method implementation

--- app/src/main/java/fr/univpau/android/quelpriximmo/MutationContent.java
+++ app/src/main/java/fr/univpau/android/quelpriximmo/MutationContent.java
@@ -1,5 +1,8 @@
 package fr.univpau.android.quelpriximmo;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,11 +19,32 @@ public class MutationContent {
 
     private static final int COUNT = 5;
 
-    static {
+    public static void addSampleItems() {
         // Add some sample items.
         for (int i = 1; i <= COUNT; i++) {
             addItem(createMutationItem(i));
         }
+    }
+
+    public static MutationItem addJSON(JSONObject json) throws JSONException {
+        MutationItem mutationItem = new MutationItem();
+        JSONObject properties = json.optJSONObject("properties");
+        if(properties != null) {
+            mutationItem.valeur_fonciere = properties.optString("valeur_fonciere", "");
+            mutationItem.nature_mutation = properties.optString("nature_mutation", "");
+            mutationItem.suffixe_numero = properties.optString("suffixe_numero", "");
+            mutationItem.numero_voie = properties.optString("numero_voie", "");
+            mutationItem.type_voie = properties.optString("type_voie", "");
+            mutationItem.voie = properties.optString("voie", "");
+            mutationItem.date_mutation = properties.optString("date_mutation", "");
+            GoodContent.GoodItem good = new GoodContent.GoodItem();
+            good.type_local = properties.optString("type_local", "");
+            good.surface_relle_bati = properties.optString("surface_relle_bati", "");
+            good.nb_pieces_principales = properties.optString("nombre_pieces_principales", "");
+            mutationItem.good_list.add(good);
+            ITEMS.add(mutationItem);
+        }
+        return mutationItem;
     }
 
     private static MutationItem createMutationItem(int position) {
@@ -33,7 +57,6 @@ public class MutationContent {
         mutationItem.type_voie = "RUE";
         mutationItem.voie = "DE L'EGLISE";
         mutationItem.date_mutation = "14/12/2015";
-        mutationItem.good_list = GoodContent.ITEMS;
         return mutationItem;
     }
 
@@ -53,7 +76,16 @@ public class MutationContent {
 
         @Override
         public String toString() {
-            return id;
+            return "MutationItem{" +
+                "id='" + id + '\'' +
+                ", valeur_fonciere='" + valeur_fonciere + '\'' +
+                ", nature_mutation='" + nature_mutation + '\'' +
+                ", date_mutation='" + date_mutation + '\'' +
+                ", suffixe_numero='" + suffixe_numero + '\'' +
+                ", numero_voie='" + numero_voie + '\'' +
+                ", type_voie='" + type_voie + '\'' +
+                ", voie='" + voie + '\'' +
+                '}';
         }
     }
 }
