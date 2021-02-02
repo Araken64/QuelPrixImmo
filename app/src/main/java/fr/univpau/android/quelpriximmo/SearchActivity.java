4fca2a4170790931c1c57291b935748d0bfd8789
Araken64 <elian.araken@gmail.com>
2021-02-02 23:09:34 +0100

    manager with authorisations

--- app/src/main/java/fr/univpau/android/quelpriximmo/SearchActivity.java
+++ app/src/main/java/fr/univpau/android/quelpriximmo/SearchActivity.java
@@ -1,32 +1,27 @@
 package fr.univpau.android.quelpriximmo;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.*;
 
-import android.widget.ImageButton;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
 
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Spinner;
 
 import fr.univpau.android.quelpriximmo.listeners.ButtonSearchListener;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.ExecutionException;
 
 import static fr.univpau.android.quelpriximmo.PositionManager.getPositionViaGPS;
 
@@ -35,12 +30,14 @@ public class SearchActivity extends AppCompatActivity {
     public static double longitude;
     public static int range = 500;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_search);
+        requestPermissions(new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
         Button button_search = (Button) findViewById(R.id.button_search);
         button_search.setOnClickListener(new ButtonSearchListener(this));
@@ -48,11 +45,6 @@ public class SearchActivity extends AppCompatActivity {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
         range = pref.getInt("distance", 500);
         Log.i("DIST", String.valueOf(range));
-        Location position = getPositionViaGPS(this);
-        Log.i("GPS", "Latitude = " + position.getLatitude());
-        Log.i("GPS", "Longitude" + position.getLongitude());
-        latitude = position.getLatitude();
-        longitude = position.getLongitude();
 
         ImageButton button_param = findViewById(R.id.imageButton_param);
         button_param.setOnClickListener(new View.OnClickListener() {
@@ -62,5 +54,34 @@ public class SearchActivity extends AppCompatActivity {
                 view.getContext().startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                android.util.Log.d("elian", "requestcode 1");
+                Location position = getPositionViaGPS(this);
+                Log.i("GPS", "Latitude = " + position.getLatitude());
+                Log.i("GPS", "Longitude" + position.getLongitude());
+                latitude = position.getLatitude();
+                longitude = position.getLongitude();
+            } else {
+                android.util.Log.d("elian", "else");
+                Toast.makeText(this, "Permission denied to use location", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private boolean hasAlreadyPermission(String permission) {
+        int result = ContextCompat.checkSelfPermission(this, permission);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
